3d4a32e6ea1f71e15b9e70af2b461a5f15eda897
davitchanturia <chanturiadato@gmail.com>
2021-09-15 22:11:41 +0400

    test

--- app.js
+++ app.js
@@ -1,35 +1,52 @@
 $(function(){
     
-
-    let pageNum = $('.pagenum').attr('id')
-
-    $('.start').click(function(){
-        // am kodiT vaqrob start buttons
-
-        $('.starter').removeClass('starter').next().addClass('starter').removeClass('blanke')
-        $('.starter-butt').remove();
-        $('.logo').attr("src", "img/rect.png");
-        $('.logo').animate({
-            'width' : '622px' , 
-            'height' : '75px' ,
-            'border-radius' : '0px!important' ,
-            'bottom' : '100px' ,
-            'left' : '50px' ,
-            'opacity' : '0.6!important' , 
-        })
-        
-        // am kodiT vnomravt gverdebs
-        $('.pagenum').text(pageNum);
-        
+    $('.next').click(function(){
+        $('.same').removeClass('active-page').next().addClass('active-page');
     })
 
-    // es kodi uzrunvelyofs ro tu pirvel gverdzea momxmarebeli ukan gadmosvlis gilaki ar mushaobdes
-    if(pageNum = 1){
-        $('.back').css({
-            'display' : 'none'
-        })
-    }
-
-
-
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let pageNum = $('.pagenum').attr('id')
+
+//     $('.next').click(function(){
+        
+// // gverdebis gadacvla
+//         $('.same').removeClass('active-page').next().addClass('active-page');
+//         // $('.starter-butt').remove();
+//         $('.logo').attr("src", "img/rect.png");
+//         $('.logo').animate({
+//             'width' : '622px' , 
+//             'height' : '75px' ,
+//             'border-radius' : '0px!important' ,
+//             'bottom' : '100px' ,
+//             'left' : '50px' ,
+//             'opacity' : '0.6!important' , 
+//         })
+        
+//         // am kodiT vnomravt gverdebs
+//         $('.pagenum').text(pageNum);
+        
+//     })
+
+//     // es kodi uzrunvelyofs ro tu pirvel gverdzea momxmarebeli ukan gadmosvlis gilaki ar mushaobdes
+//     if(pageNum = 1){
+//         $('.back').css({
+//             'display' : 'none'
+//         })
+//     }
+    
+   
+
