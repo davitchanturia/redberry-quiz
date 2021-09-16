4f44a9d0d863797ffc51a7a62100e43ee3d8f437
davitchanturia <chanturiadato@gmail.com>
2021-09-16 19:01:25 +0400

    newedit

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
 $(function(){
-    
-    $('.next').click(function(){
-        $('.same').removeClass('active-page').next().addClass('active-page');
+
+    // x ცვლადს ვქმნით რომ გვერდებზე გადასვლისას ბოლოდან ისევ პირველზე და პირველიდან ბოლოზე არ დაგვაბრუნოს
+    let x = 0; 
+
+    let pageNum = $('.pagenum').attr('id')
+
+    // gverdebze gadasvla
+    $('.next').click(function(e){
+        if(x < 4){
+            $('.active-page').removeClass('active-page').next().addClass('active-page');
+            x++;
+
+           let test = checkPage();
+            
+        }
+        
     })
+    $('.back').click(function(){
+        if(x > 1){
+            $('.active-page').removeClass('active-page').prev().addClass('active-page');
+            x--;
+            
+            let sda = checkPage();
+            
+        }
+    })
+    
+
+     let checkPage = function(){
+         // უიკან გადასასვლელი ღილაკის გაქრობა
+         if(x == 1 ){
+            $('.back').css({
+                'display' : 'none'
+            });
+        }else{
+            $('.back').css({
+                'display' : 'inline-block'
+            });
+        }
+
+        // წინ გადასასვლელი ღილაკის გაქრობა ბოლო გვერდზე
+        if(x == 4 ){
+            $('.next').css({
+                'display' : 'none'
+            });
+        }else{
+            $('.back').css({
+                'display' : 'inline-block'
+            });
+        }
+     }
 
 })
 
@@ -19,34 +66,6 @@ $(function(){
 
 
 
-//     let pageNum = $('.pagenum').attr('id')
 
-//     $('.next').click(function(){
-        
-// // gverdebis gadacvla
-//         $('.same').removeClass('active-page').next().addClass('active-page');
-//         // $('.starter-butt').remove();
-//         $('.logo').attr("src", "img/rect.png");
-//         $('.logo').animate({
-//             'width' : '622px' , 
-//             'height' : '75px' ,
-//             'border-radius' : '0px!important' ,
-//             'bottom' : '100px' ,
-//             'left' : '50px' ,
-//             'opacity' : '0.6!important' , 
-//         })
-        
-//         // am kodiT vnomravt gverdebs
-//         $('.pagenum').text(pageNum);
-        
-//     })
-
-//     // es kodi uzrunvelyofs ro tu pirvel gverdzea momxmarebeli ukan gadmosvlis gilaki ar mushaobdes
-//     if(pageNum = 1){
-//         $('.back').css({
-//             'display' : 'none'
-//         })
-//     }
-    
    
 
