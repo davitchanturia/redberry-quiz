173eda510db07a3e022c7e04ee5009da17001890
davitchanturia <chanturiadato@gmail.com>
2021-09-17 18:35:45 +0400

    lvl6
    მეორე ფეიჯის კითხვების ფუნქციონალი თითქმის დაწერილია

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    // let input = $('.inputs')''
     let input1 = $('#first-input');
     let input2 = $('#second-input');
     let input3 = $('#third-input');
@@ -10,7 +11,7 @@ $(function(){
     let pageNum = $('.pagenum').attr('id')
 
     // გვერდებზე გადასვლა + ვალიდაცია
-    $(document).on('click' , '.next' , function(e){
+    $('.next').on('click' , function(e){
         e.preventDefault()
         
         if(x < 4){
@@ -30,25 +31,121 @@ $(function(){
 
                         if( valstring[1] == "redberry.com" ){
                             $('.next').prop('disabled', false);
+                            $('.email-error').html('<i class="bi bi-check-lg" style="color:green!important;" ></i>');
                         }else{
                             $('.next').prop('disabled', true);
-                            $('.email-error').text('იმეილი არასწორია')
+                            $('.email-error').text('გთხოვთ დარეგისტრირდეთ რედბერის მეილით');
                         }
+                })
+            }
+
+            if( x == 2){
+                $(document).on('change' , '.haveCovid' , function(){
+
+                    // პირველი შეკითხვის value
+                    var mosanishniCovid = $('.haveCovid[type=radio]:checked').val();
+          
+                    console.log(mosanishniCovid)
+
+                    if(mosanishniCovid == "yes"){
+                        $('.left-content').append(`
+                            
+                        <div class="question-div">
+    
+                            <h2>ანტისხეულების ტესტი გაქვს გაკეთებული?*</h2>
+    
+                            <div class="question-child">
+                                <input type="radio" id="yes" name="c" value="yes" class="havetest">
+                                <label for="yes">კი</label>
+                             </div>
+                          
+                             <div class="question-child">
+                                 <input type="radio" id="no" name="c" value="no" class="havetest">
+                                 <label for="no">არა</label>
+                             </div>
+
+                          
+                          </div>
+    
+                          
+                        `);
+                                       
+                                      $(document).on('change' , '.havetest' , function(){
+
+                                                // მეორე შეკითხვის value
+                                                var mosanishniTest = $('.havetest[type=radio]:checked').val();
+                                            console.log(mosanishniTest)
+
+                                                if(mosanishniTest == "yes"){
+                                                    $('.left-content').append(`
+                                                        
+                                                    <div class="question-div">
+                                
+                                                        <h2>თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი რიცხვი და ანტისხეულების რაოდენობა*</h2>
+                                
+                                                        <div class="question-child">
+                                                            <input type="date" id="yes" name="d" value="05/07/1999" class="haveAnti num-input" placeholder="რიცხვი">
+                                                            
+                                                         </div>
+                                                      
+                                                         <div class="question-child">
+                                                             <input type="number" id="no" value="2131" name="d" class="haveAnti num-input" style="margin-top:10px;">
+                                                             
+                                                         </div>
+                            
+                                                      
+                                                      </div>
+                                
+                                                      
+                                                    `);
+                        
+                                            }
+                                        })
+
+                    }
+
 
                 })
-            
-            
-            
-            
-                //    $('.form1').submit(function(e){
-            //        if(input1.val() == "123"){
-            //         // $('.active-page').removeClass('active-page').next().addClass('active-page');
-            //         // x++;
-            //        }else{
-            //             $('.next').attr("disabled", true);
-            //         }
-            //    })
-                         
+            }
+
+            if( x == 3){
+                
+                $(document).on('change' , '.haveCovid' , function(){
+
+                    // პირველი შეკითხვის value
+                    var mosanishniCovid = $('.haveCovid[type=radio]:checked').val();
+          
+                    console.log(mosanishniCovid)
+
+                    if(mosanishniCovid == "yes"){
+                        $('.left-content').append(`
+                            
+                        <div class="question-div">
+    
+                            <h2>ანტისხეულების ტესტი გაქვს გაკეთებული?*</h2>
+    
+                            <div class="question-child">
+                                <input type="radio" id="yes" name="c" value="yes" class="havetest">
+                                <label for="yes">კი</label>
+                             </div>
+                          
+                             <div class="question-child">
+                                 <input type="radio" id="no" name="c" value="no" class="havetest">
+                                 <label for="no">არა</label>
+                             </div>
+
+                          
+                          </div>
+    
+                          
+                        `);
+                                       
+                                      
+
+                    }
+
+
+                })
 
             }
 
@@ -61,72 +158,11 @@ $(function(){
             $('.active-page').removeClass('active-page').prev().addClass('active-page');
             x--;
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            // let sda = checkPage();
-            
         }
     })
-    
-
-    //  let checkPage = function(){
-    //      // უიკან გადასასვლელი ღილაკის გაქრობა
-    //      if(x == 1 ){
-    //         $('.back').css({
-    //             'display' : 'none'
-    //         });
-    //     }else{
-    //         $('.back').css({
-    //             'display' : 'inline-block'
-    //         });
-    //     }
-
-    //     // წინ გადასასვლელი ღილაკის გაქრობა ბოლო გვერდზე
-    //     if(x == 4 ){
-    //         // $('.next').css({
-    //         //     'display' : 'none'
-    //         // });
-    //     }else{
-    //         $('.back').css({
-    //             'display' : 'inline-block'
-    //         });
-    //     }
-    //  }
-
-
-// ეს კოდი უზრუნველყოფს რო რედბერის მეილის გარდა სხვა რამით არ შევიდეს
-    let splitString = function(){
-        let string = $(this).val();
-
-        let splitString = string.split('@')
-    }
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
    
 
