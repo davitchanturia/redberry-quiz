ec0bb294a6ff7f966e6d711f8bfa8826ac89aca4
davitchanturia <chanturiadato@gmail.com>
2021-09-18 09:42:03 +0400

    lvl8

--- app.js
+++ app.js
@@ -40,68 +40,100 @@ $(function(){
             }
 
             if( x == 2){
+
                 $(document).on('change' , '.haveCovid' , function(){
 
-                    // პირველი შეკითხვის value
                     var mosanishniCovid = $('.haveCovid[type=radio]:checked').val();
-          
-                    console.log(mosanishniCovid)
 
+                    // თუ კოვიდი გადატანილი აქვს
                     if(mosanishniCovid == "yes"){
-                        $('.left-content').append(`
-                            
-                        <div class="question-div">
+                        
+                        // ვქმნით ანტისხეულების ტესტის შესახებ კითხვას
+                           $('.answers').append(`
+                               
+                           <div class="question-div havetest">
     
-                            <h2>ანტისხეულების ტესტი გაქვს გაკეთებული?*</h2>
+                               <h2>ანტისხეულების ტესტი გაქვს გაკეთებული?*</h2>
     
-                            <div class="question-child">
-                                <input type="radio" id="yes" name="c" value="yes" class="havetest">
-                                <label for="yes">კი</label>
-                             </div>
-                          
-                             <div class="question-child">
-                                 <input type="radio" id="no" name="c" value="no" class="havetest">
-                                 <label for="no">არა</label>
-                             </div>
+                               <div class="question-child">
+                                   <input type="radio" id="yes" name="c" value="yes" class="havetest">
+                                   <label for="yes">კი</label>
+                                </div>
+                             
+                                <div class="question-child">
+                                    <input type="radio" id="no" name="c" value="no" class="havetest">
+                                    <label for="no">არა</label>
+                                </div>
 
-                          
-                          </div>
-    
-                          
-                        `);
-                                       
-                                      $(document).on('change' , '.havetest' , function(){
+                             </div>
+                           `);
+                                      
+                            $(document).on('change' , '.havetest' , function(){
 
-                                                // მეორე შეკითხვის value
+                                                
                                                 var mosanishniTest = $('.havetest[type=radio]:checked').val();
-                                            console.log(mosanishniTest)
 
+                                                 //  თუ ანტისხეულების ტესტი გაკეთებული აქვს
                                                 if(mosanishniTest == "yes"){
-                                                    $('.left-content').append(`
+
+                                                    // ვქმნით ორ ინფუთს - თარიღი და რაოდენობა
+                                                    $('.answers').append(`
                                                         
-                                                    <div class="question-div">
+                                                    <div class="question-div haveAnti">
                                 
                                                         <h2>თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი რიცხვი და ანტისხეულების რაოდენობა*</h2>
                                 
                                                         <div class="question-child">
-                                                            <input type="date" id="yes" name="d" value="05/07/1999" class="haveAnti num-input" placeholder="რიცხვი">
-                                                            
+                                                            <input type="date" id="yes" name="d"  class="haveAnti num-input" placeholder="რიცხვი">
                                                          </div>
                                                       
                                                          <div class="question-child">
-                                                             <input type="number" id="no" value="2131" name="d" class="haveAnti num-input" style="margin-top:10px;">
-                                                             
+                                                             <input type="number" id="no" value="2131" name="d" class="haveAnti num-input" style="margin-top:10px;">   
                                                          </div>
                             
                                                       
                                                       </div>
+                                                      
                                 
                                                       
                                                     `);
-                        
-                                            }
-                                        })
+                                                    // ვშლით სხვა პასუხის შესაძლო გაგრძელებებს უკან მიბრუნებისას ბევრჯერ რო არ შექმნას დივი
 
+                                                    $('.period').remove();
+
+
+
+                                                // თუ ტესტი გაკეთებული არ აქვს
+                                                 }
+                                                 if(mosanishniTest == "no"){
+
+                                                    // ვქმნით ინფუთს - გადატანის პერიოდი
+                                                    $('.answers').append(`
+                                                        
+                                                    <div class="question-div period">
+                                
+                                                        <h2>მიუთითე მიახლოებითი პერიოდი (დღე/თვე/წელი) როდის გქონდა Covid-19*</h2>
+                            
+                                                        <div class="question-child">
+                                                            <input type="date" id="yes" name="d" value="05/07/1999" class="period num-input" placeholder="რიცხვი">
+                                                         </div>
+
+                                                      </div>
+                                
+                                                      
+                                                    `)
+
+                                                    // ვშლით სხვა პასუხის შესაძლო გაგრძელებებს უკან მიბრუნებისას ბევრჯერ რო არ შექმნას დივი
+
+                                                    $('.haveAnti').remove();
+
+                                                 }
+                              })
+
+                    }else{
+                        // ვშლით სხვა პასუხის შესაძლო გაგრძელებებს უკან მიბრუნებისას ბევრჯერ რო არ შექმნას დივი
+                        $('.answers').empty();
+                        
                     }
 
 
@@ -110,46 +142,44 @@ $(function(){
 
             if( x == 3){
                 
-                $(document).on('change' , '.haveCovid' , function(){
+                $(document).on('change' , '.haveVaccine' , function(){
 
                     // პირველი შეკითხვის value
-                    var mosanishniCovid = $('.haveCovid[type=radio]:checked').val();
-          
-                    console.log(mosanishniCovid)
+                    var mosanishniVaccine = $('.haveVaccine[type=radio]:checked').val();
 
-                    if(mosanishniCovid == "yes"){
-                        $('.left-content').append(`
+                    console.log(mosanishniVaccine)
+                    if(mosanishniVaccine == "yes"){
+                        y++;
+                        $('.left-cont').append(`
                             
                         <div class="question-div">
     
-                            <h2>ანტისხეულების ტესტი გაქვს გაკეთებული?*</h2>
+                            <h2>აირჩიე რა ეტაპზე ხარ*</h2>
     
                             <div class="question-child">
-                                <input type="radio" id="yes" name="c" value="yes" class="havetest">
-                                <label for="yes">კი</label>
+                                <input type="radio" id="yes" name="m" value="first-Doze" class="">
+                                <label for="first-Doze">პირველი დოზა და დარეგისტრირებული ვარ მეორეზე</label>
                              </div>
                           
                              <div class="question-child">
-                                 <input type="radio" id="no" name="c" value="no" class="havetest">
-                                 <label for="no">არა</label>
+                                 <input type="radio" id="no" name="m" value="full-vacinated" class="">
+                                 <label for="full-vacinated">სრულად აცრილი ვარ</label>
                              </div>
+
+                             <div class="question-child">
+                             <input type="radio" id="no" name="m" value="only-first" class="link">
+                             <label for="only-first">პირველი დოზა და არ დავრეგისტრირებულვარ მეორეზე</label>
+                         </div>
 
                           
                           </div>
     
                           
                         `);
-                                       
-                                      
 
                     }
-
-
                 })
-
-            }
-
-            
+            }   
         }
         
     })
